4b26851fe6b0865e8a8bd6d0e92c8ca8bcdf6509
Rafael De Oliveira <sdeoliveira.r@gmail.com>
2021-01-08 02:25:05 -0300

    Project created

---
+++ backend/src/test/java/com/dvsuperior/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
